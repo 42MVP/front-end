9dd711c07ec616d3d17fd48ec70238f88dab78af
chaejin-jen <jen71324@gmail.com>
2023-07-09 10:04:45 +0900

    ✨ FEAT: game view - game screen

---
+++ src/components/gameview-components/GameScreen.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="game-ui-continer">
+    <div class="user-info-div">
+      <ProfileCard user_id="chaejkim" img="1.png" img_size="100" />
+    </div>
+    <span class="score-div">{{ 5 }} : {{ 4 }}</span>
+    <div class="user-info-div">
+      <ProfileCard user_id="hyeongki" img="4.png" img_size="100" />
+    </div>
+  </div>
+  <div class="table-div">
+    <div class="stick-div" />
+    <div class="net-div" />
+    <div class="stick-div" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import ProfileCard from '../profileview-components/ProfileCard.vue';
+</script>
+
+<style scoped>
+.game-ui-continer {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 0px 50px;
+  min-width: 500px;
+  .user-info-div {
+    font: var(--medium);
+  }
+
+  .score-div {
+    font: var(--extra-large);
+    align-self: center;
+  }
+}
+
+.table-div {
+  display: flex;
+  border-style: solid;
+  justify-content: space-between;
+  border-color: var(--semi-brown);
+  border-width: 2px;
+  border-radius: 15px;
+  margin: 10px 50px;
+  min-width: 1000px;
+  min-height: 500px;
+  background-color: var(--base-ivory);
+
+  .net-div {
+    border: 1px dashed;
+    width: 1px;
+    height: 100%;
+    border-color: var(--base-gray);
+  }
+  .stick-div {
+    background-color: var(--brown);
+    width: 10px;
+    height: 60px;
+    margin: 5px 10px;
+  }
+}
+</style>
--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -2,14 +2,21 @@
   <div v-if="!isMatched" class="container">
     <GameMatching />
   </div>
+  <div v-else class="container">
+    <div class="game-screen-div" v-if="isGameConnected">
+      <GameScreen />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
 
 import GameMatching from '@/components/gameview-components/GameMatching.vue';
+import GameScreen from '@/components/gameview-components/GameScreen.vue';
 
-const isMatched = ref(0);
+const isMatched = ref(1);
+const isGameConnected = ref(1);
 </script>
 
 <style scoped>
@@ -18,6 +25,13 @@ const isMatched = ref(0);
   justify-content: center;
   align-items: center;
   box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+}
+
+.game-screen-div {
+  display: flex;
+  flex-direction: column;
   width: 100%;
   height: 100%;
 }
