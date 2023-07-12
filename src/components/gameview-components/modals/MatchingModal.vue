4ed9b9a4a9bf7fa4706c940c0c9ba19b2f04943c
chaejin-jen <jen71324@gmail.com>
2023-07-12 14:53:36 +0900

    💚 CHORE : add spinner in to MatchingModal

--- src/components/gameview-components/modals/MatchingModal.vue
+++ src/components/gameview-components/modals/MatchingModal.vue
@@ -1,6 +1,8 @@
 <template>
   <Modal title="매칭중">
-    <template #body> </template>
+    <template #body>
+      <div id="spinner"></div>
+    </template>
     <template #footer>
       <Button class="button-div" text="취소" @response="cancle" />
     </template>
@@ -9,6 +11,7 @@
 
 <script setup lang="ts">
 import Modal from '@/components/Modal.vue';
+import GameLoading from '../GameLoading.vue';
 import Button from '@/components/BasicButton.vue';
 
 const emits = defineEmits(['response']);
@@ -28,5 +31,28 @@ const tmp = () => {
   align-items: center;
   justify-content: center !important;
   margin: 0 100px;
+}
+
+#spinner {
+  display: flex;
+  align-items: center;
+  justify-content: center !important;
+  margin: 0 80px;
+
+  margin: clac(50% - 25px) auto;
+  width: 100px;
+  height: 100px;
+  border: 7px solid #e0afa0;
+  box-sizing: border-box;
+  border-top-color: #fff;
+
+  border-radius: 100%;
+  animation: spin 1s ease-in-out infinite;
+}
+
+@keyframes spin {
+  100% {
+    transform: rotate(360deg);
+  }
 }
 </style>
