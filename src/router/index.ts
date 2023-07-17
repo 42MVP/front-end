e55ff3369f439598b6b15b9c2d6243030fb0a9e5
chaejin-jen <jen71324@gmail.com>
2023-07-17 20:59:45 +0900

    ♻️  REFACTOR: router path

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import HomeView from '@/views/HomeView.vue';
 import FriendsListView from '@/views/FriendsListView.vue';
-import GameView from '@/views/GameView.vue';
-import SignInView from '@/views/SignInView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,17 +8,17 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: () => import('@/views/HomeView.vue'),
+      component: HomeView,
     },
     {
       path: '/signIn',
       name: 'signIn',
-      component: () => import('../views/SignInView.vue'),
+      component: () => import('@/views/SignInView.vue'),
     },
     {
       path: '/signUp',
       name: 'signUp',
-      component: () => import('../views/SignUpView.vue'),
+      component: () => import('@/views/SignUpView.vue'),
       redirect: 'signUp/setProfile',
       children: [
         {
@@ -44,13 +42,13 @@ const router = createRouter({
       path: '/users',
       name: 'users',
 
-      component: () => import(/* webpackChunkName: "user" */ '@/views/MyProfileView.vue'),
+      component: () => import('@/views/MyProfileView.vue'),
     },
     {
       path: '/users/:id',
       name: 'profile',
 
-      component: () => import(/* webpackChunkName: "user" */ '@/views/ProfileView.vue'),
+      component: () => import('@/views/ProfileView.vue'),
       props: route => ({ user_id: route.params.id }),
     },
     {
