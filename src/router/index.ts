29146a70cc4fb33c7047c8857c731d165d5f1631
chaejin-jen <jen71324@gmail.com>
2023-07-20 16:09:24 +0900

    ♻️  REFACTOR: router path - import dynamic

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '@/views/HomeView.vue';
-import FriendsListView from '@/views/FriendsListView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +6,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: import('@/views/HomeView.vue'),
     },
     {
       path: '/signIn',
@@ -59,7 +57,7 @@ const router = createRouter({
     {
       path: '/friends',
       name: 'friends',
-      component: FriendsListView,
+      component: () => import('@/views/FriendsListView.vue'),
     },
   ],
 });
