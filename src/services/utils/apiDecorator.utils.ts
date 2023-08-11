bdfea591ebfe1f881ab370ffe076bcdef16e562a
kanghyki <kanghyki@gmail.com>
2023-08-11 16:16:59 +0900

    🐛 FIX: decorator token sync

--- src/services/utils/apiDecorator.utils.ts
+++ src/services/utils/apiDecorator.utils.ts
@@ -21,16 +21,12 @@ export function APIWithToken() {
     const fn = descriptor.value;
     descriptor.value = async function (...args: any) {
       const accessToken = Cookies.get('access-token');
-      if (!accessToken) {
-        throw '로그인이 필요합니다.';
-        return;
-      } else if (isExpiredToken(accessToken)) {
+      if (isExpiredToken(accessToken)) {
         await getNewAccessToken();
       }
       try {
         return await fn.apply(target, args);
       } catch (e) {
-        if (e.response.request.status === 401) await getNewAccessToken();
         throw `요청 에러: ${e}`;
       }
     };
