a1d548b355b2416565f6a398e345997e6050f931
KangHyunKim <kanghyki@gmail.com>
2023-08-23 13:52:29 +0900

    FEAT: set profile (#76)

--- src/services/login.service.ts
+++ src/services/login.service.ts
@@ -1,11 +1,10 @@
-import { APIWithToken, getUserID } from '@/services/utils/apiDecorator.utils';
+import { APIWithToken } from '@/services/utils/apiDecorator.utils';
 import { axiosAPI } from './utils/axiosInstance.utils';
 
 export class LoginService {
   @APIWithToken()
   static async getUserInfo() {
-    const userID = getUserID();
-    const ret = await axiosAPI.auth().get(`http://localhost:3000/user/${userID}`);
+    const ret = await axiosAPI.auth().get(`http://localhost:3000/user/me`);
     return ret;
   }
 }
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,7 +1,6 @@
 import { APIWithToken } from '@/services/utils/apiDecorator.utils';
 import { axiosAPI } from '@/services/utils/axiosInstance.utils';
 import type { User } from '@/interfaces/user/User.interface';
-import type { UserInfo } from '@/interfaces/user/UserInfo.interface';
 
 export class UserService {
   static mockUser = (): User[] => {
@@ -25,23 +24,7 @@ export class UserService {
   };
 
   @APIWithToken()
-  static async getAllUser(): Promise<User> {
-    const ret = await axiosAPI.auth().get('/user');
-    return ret.data;
-  }
-
-  @APIWithToken()
-  static async getUser(username: string): Promise<User> {
-    const ret = await axiosAPI.auth().get('/user', {
-      params: { username: username },
-    });
-    return ret.data;
-  }
-
-  // TODO: TYPE
-  @APIWithToken()
-  static async getUserById(id: number) {
-    const ret = await axiosAPI.auth().get(`/user/${id}`);
-    return ret.data;
+  static async setUserProfile(formData: FormData) {
+    return axiosAPI.auth().put(`/user`, formData);
   }
 }
