bd07ea20721f560314a47b070e14b976795bbbfa
Chaejin Kim <73324850+chaejin-jen@users.noreply.github.com>
2023-09-07 14:33:27 +0900

    ✨FEAT: 게임 보완 (#94)
    * ♻️  REFACTOR: Restructure gamePlay
    
    * ✨ FEAT: Add Countdown before gameStart
    
    * 💚 CHORE: Unflag startTimeMs, isStarted
    
    * [FEAT] 게임 결과 모달 (#96)
    
    * [FEAT] game results
    
    * [FIX] game history interface
    
    * [FIX] game Modal 시점
    
    * [ADD]result Modal에 game rate 추가
    
    * [FEAT] 모달 확인 버튼
    
    * 피드백 기반 수정
    
    * [APPROVE] 모달 css 수정
    
    * fix tmp
    
    * fix tmp code
    
    * ✨ FEAT: Add game force quit
    
    * Fix: Change the matching status when moving to game
    
    ---------
    
    Co-authored-by: Hee Ju Jang <76811061+ittzggd@users.noreply.github.com>
    Co-authored-by: kanghyki <kanghyki@gmail.com>

---
+++ src/components/gameview-components/GameResultModal.vue
@@ -0,0 +1,92 @@
+<template>
+  <Modal title="경기 결과">
+    <template #body>
+      <div class="body-container">
+        <div class="user-info-div">
+          <span class="bold">Win👑</span>
+          <AvatarItem :username="gameResult?.winner" :avartarUrl="gameResult?.winnerAvatarUrl" imgSize="100">
+            <span class="rate">{{ gameResult?.winnerRate as number }} (+12)</span>
+          </AvatarItem>
+        </div>
+        <span class="score">{{ gameResult?.winnerScore }} : {{ gameResult?.loserScore }} </span>
+        <div class="user-info-div">
+          <span class="bold">Lose🍂</span>
+          <AvatarItem :username="gameResult?.loser" :avartarUrl="gameResult?.loserAvatarUrl" imgSize="100">
+            <span class="rate">{{ gameResult?.loserRate as number }} (-12)</span>
+          </AvatarItem>
+        </div>
+      </div>
+    </template>
+    <template #footer>
+      <BasicButton text="확인" @click="moveToProfile()" />
+    </template>
+  </Modal>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import Modal from '@/components/Modal.vue';
+import AvatarItem from '@/components/common/AvatarItem.vue';
+import BasicButton from '../BasicButton.vue';
+
+import { useGameStore } from '@/stores/game.store';
+import { GameService } from '@/services/game.service';
+import type { GameHistory } from '@/interfaces/game/GameHistory.interface';
+import { useRouter } from 'vue-router';
+import { useLoginStore } from '@/stores/login.store';
+
+const loginStore = useLoginStore();
+const gameStore = useGameStore();
+const gameResult = ref<GameHistory>();
+
+const router = useRouter();
+
+const moveToProfile = () => {
+  router.push(`/users/${loginStore?.name}`);
+};
+
+onMounted(async () => {
+  try {
+    gameResult.value = await GameService.getGameResult(gameStore.gameHistoryId);
+    // gameResult.value = await GameService.getGameResult(gameStore.matchInfo!.gameRoomId!);
+  } catch (e) {
+    console.log(e);
+  }
+});
+</script>
+
+<style scoped>
+.body-container {
+  display: flex;
+  flex-flow: row;
+  align-items: center;
+  text-align: center;
+}
+
+.user-info-div {
+  margin: 0 80px;
+  display: flex;
+  flex-flow: column;
+  text-align: center;
+  font: var(--large);
+}
+
+.score {
+  font: var(--extra-large);
+  align-self: center;
+  font-weight: bolder;
+}
+
+.rate {
+  align-self: center;
+  font-weight: lighter;
+  font-size: medium;
+}
+
+.button-div {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 50px;
+}
+</style>
